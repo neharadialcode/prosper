286971445fb5b1f4c258134dd79e89bd50d2c0c2
Neha <neharadialcode@gmail.com>
2024-02-11 11:34:33 +0530

    components added app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Predict from "./components/Predict";
+import Advantages from "./components/Advantages";
+import Architecture from "./components/Architecture";
+import Liquidity from "./components/Liquidity";
+import Token from "./components/Token";
+import Prediction from "./components/Prediction";
+import CustomPool from "./components/CustomPool";
+import OurBackers from "./components/OurBackers";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <Hero />
+      <Predict />
+      <Advantages />
+      <Architecture />
+      <Liquidity />
+      <Token />
+      <Prediction />
+      <CustomPool />
+      <OurBackers />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="flex">
+        <div>dfghj</div>
+        <div>dfgnhm,</div>
+        <div>Back to top</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
