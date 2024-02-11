14d1336fd27c288db9fa4d6d51142866f8f32208
Neha <neharadialcode@gmail.com>
2024-02-11 20:27:47 +0530

    meta tags added

--- src/components/Predict.jsx
+++ src/components/Predict.jsx
@@ -10,7 +10,6 @@ const Predict = () => {
           <iframe
             className="md:min-h-[450px] min-h-[200px] sm:min-h-[350px] sm:h-[350px]"
             id="youtubeiframe300644794"
-            className="t347__iframe"
             width="100%"
             height="350px"
             src="https://www.youtube.com/embed/H86OGVc9Pj8?autoplay=1&amp;rel=0"
