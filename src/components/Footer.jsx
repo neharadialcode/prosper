25ac947e8b99541a1c2b196be6d3fe0958423443
Neha <neharadialcode@gmail.com>
2024-02-11 11:50:58 +0530

    footer content added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { footerLinks } from "./Helper";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <div className="flex">
-        <div>dfghj</div>
-        <div>dfgnhm,</div>
-        <div>Back to top</div>
+    <div className="px-10">
+      <div className="flex justify-between items-center">
+        <div className="whitespace-nowrap">© {year} Prosper.so</div>
+        <ul className="flex px-[10%] flex-wrap justify-center ">
+          {footerLinks.map((obj, i) => (
+            <li key={i} className="px-4 whitespace-nowrap text-center py-1">
+              <a className="text-center hover:opacity-70" href={obj.url}>
+                {obj.link}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="whitespace-nowrap cursor-pointer">Back to top</div>
       </div>
     </div>
   );
--- src/components/Helper.jsx
+++ src/components/Helper.jsx
@@ -1,7 +1,50 @@
-import React from "react";
-
-const Helper = () => {
-  return <div>Helper</div>;
-};
-
-export default Helper;
+export const footerLinks = [
+  {
+    link: "Instruction",
+    url: "#Instruction",
+  },
+  {
+    link: "Advantages",
+    url: "#Advantages",
+  },
+  {
+    link: "Architecture",
+    url: "#Architecture",
+  },
+  {
+    link: "Liquidity program",
+    url: "#Liquidity program",
+  },
+  {
+    link: "Token utility",
+    url: "#Token utility",
+  },
+  {
+    link: "Prediction insurance",
+    url: "#Prediction insurance",
+  },
+  {
+    link: "Custom pools",
+    url: "#Custom pools",
+  },
+  {
+    link: "What's next",
+    url: "#What's next",
+  },
+  {
+    link: "Backers & partners",
+    url: "#Backers & partners",
+  },
+  {
+    link: "Medium",
+    url: "#Medium",
+  },
+  {
+    link: "Prosper's rules",
+    url: "#Prosper's rules",
+  },
+  {
+    link: "How to start",
+    url: "#How to start",
+  },
+];
